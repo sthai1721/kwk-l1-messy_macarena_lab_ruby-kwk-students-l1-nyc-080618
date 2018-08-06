5c32a1fefe10990e057e35a8149f130b634fe6ea
sthai1721 <sophia.thai@rutgers.edu>
2018-08-06 18:16:08 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -6,25 +6,25 @@ puts ""
 # The first instruction is correct. 
 
 # Step 1: GOOD
-puts "1: Right arm out in front of you, palm facing down."
+puts "Step 1: Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
-puts "2: Left arm out in front of you, palm facing down."
+puts "Step 2: Left arm out in front of you, palm facing down."
 
 # Step 3: FIXME
-puts "3: Turn your right palm up." 
+puts "Step 3: Turn your right palm up." 
 
 # Step 4: FIXME
-puts "4: Turn your left palm up."
+puts "Step 4: Turn your left palm up."
 
 # Step 5: FIXME
-puts "5: Place your right hand on your left shoulder."
+puts "Step 5: Place your right hand on your left shoulder."
 
 # Step 6: FIXME
-puts "6: Place your left hand on your right shoulder."
+puts "Step 6: Place your left hand on your right shoulder."
 
 # Step 7: FIXME
-puts "7: Bring your right hand up and around to the back of your head." 
+puts "Step 7: Bring your right hand up and around to the back of your head." 
 
 # Step 8: FIXME
-puts "8: Bring your left hand up and around to the back of your head."
+puts "Step 8: Bring your left hand up and around to the back of your head."
